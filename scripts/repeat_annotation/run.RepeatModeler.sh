9ce3db1e6fc8274ddc199bf79ee13e4ffefe7fd6
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-03-30 22:02:33 +0200

    run RepeatModeler

---
+++ scripts/repeat_annotation/run.RepeatModeler.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+########################################################################
+
+export method=$1
+export cluster=$2
+
+#########################################################################
+
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/${USER}/HelmetedCurassowGenome
+fi
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+fi
+
+
+export pathGenomeAssembly=${path}/results/genome_assembly/${method}
+export pathResults=${path}/results/repeats/${method}
+export pathScripts=${path}/scripts/repeat_annotation
+
+#########################################################################
+
+RepeatModeler -database ${pathResults}/repeat_modeler_db -pa 8 -LTRStruct >& ${pathResults}/RepeatModeler.out 
+
+#########################################################################
