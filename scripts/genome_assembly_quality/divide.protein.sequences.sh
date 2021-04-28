298f35d71ab23a297084b86aede39d8f7b752647
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-04-28 17:31:14 +0200

    script to divide protein sequences

---
+++ scripts/genome_assembly_quality/divide.protein.sequences.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export sp=$1
+export nbparts=$2
+export cluster=$3
+
+#####################################################################
+
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/necsulea/HelmetedCurassowGenome
+else
+    if [ ${cluster} = "cloud" ]; then
+	export path=/mnt/mydatalocal/HelmetedCurassowGenome/
+    else
+	echo "unknown cluster"
+	exit
+    fi
+fi
+
+export pathProteinSequences=${path}/data/protein_sequences/${sp}
+export pathScripts=${path}/scripts/genome_assembly_quality
+
+export release=103
+
+###################################################################################
+
+if [ -e ${pathProteinSequences}/fasta_parts ]; then
+    echo "path output already there"
+else
+   mkdir ${pathProteinSequences}/fasta_parts
+fi
+
+###################################################################################
+
+perl ${pathScripts}/divide.sequences.pl --pathFastaInput=${pathProteinSequences}/AllPeptides_Ensembl${release}.fa --nbParts=${nbparts} --prefixOutput=${pathSequences}/fasta_parts/AllPeptides_Ensembl${release}
+
+###################################################################################
+
