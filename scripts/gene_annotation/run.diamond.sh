4263fae015369aac7c735d24c03e8b305ca4a12c
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-05-10 16:45:14 +0200

    changed path for penelope

--- scripts/gene_annotation/run.diamond.sh
+++ scripts/gene_annotation/run.diamond.sh
@@ -26,12 +26,20 @@ mkdir ${pathResults}/diamond_results
 for file in `ls ${pathProteins}/Ensembl103/primary_transcripts/ | grep all.fa`
 do
     export sp=`echo ${file} | cut -f 1 -d '.'`
-    
-    diamond blastp --threads 24 --evalue 0.001 --max-target-seqs 20 --id ${minid} --query-cover ${minquerycov} --subject-cover ${minsubjectcov} --query ${pathResults}/braker.faa --db ${pathProteins}/Ensembl103/primary_transcripts/${sp} --out ${pathResults}/diamond_results/${sp}_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps
+
+    if [ -e ${pathResults}/diamond_results/${sp}_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out ]; then
+	echo ${sp} "already done"
+    else
+    	diamond blastp --threads 24 --evalue 0.001 --max-target-seqs 20 --id ${minid} --query-cover ${minquerycov} --subject-cover ${minsubjectcov} --query ${pathResults}/braker.faa --db ${pathProteins}/Ensembl103/primary_transcripts/${sp} --out ${pathResults}/diamond_results/${sp}_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps
+    fi
 done 
 
 ##########################################################################
 
-diamond blastp --threads 24 --evalue 0.001  --max-target-seqs 20  --id ${minid} --query-cover ${minquerycov} --subject-cover ${minsubjectcov}  --query ${pathResults}/braker.faa --db ${pathProteins}/B10K_NCBI/Penelope_pileata --out ${pathResults}/diamond_results/Penelope_pileata_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps
+if [ -e ${pathResults}/diamond_results/Penelope_pileata_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out ]; then
+    echo "Penelope pileata already done"
+else
+    diamond blastp --threads 24 --evalue 0.001  --max-target-seqs 20  --id ${minid} --query-cover ${minquerycov} --subject-cover ${minsubjectcov}  --query ${pathResults}/braker.faa --db ${pathProteins}/B10K_NCBI/Penelope_pileata --out ${pathResults}/diamond_results/Penelope_pileata_minid${minid}_minquerycov${minquerycov}_minsubjectcov${minsubjectcov}.diamond.blastp.out --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps
+fi
 
 ##########################################################################
