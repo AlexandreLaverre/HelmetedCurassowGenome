89ef73072b871c2510f73c6376787ecbd7abe22a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-05-10 15:57:54 +0200

    run diamond on BRAKER2 predictions

---
+++ scripts/gene_annotation/build.diamond.db.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export cluster=$1
+export method="MEGAHIT_RAGOUT"
+export brakerset="BRAKER_Ensembl103_multithread"
+export genome="genome_sequence_renamed"
+
+##########################################################################
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+    export pathTools=/mnt/mydatalocal/Tools/OrthoFinder/tools
+fi
+
+export pathProteins=${path}/data/protein_sequences
+export pathGenomeAssembly=${path}/results/genome_assembly/${method}/${genome}.fa
+export pathResults=${path}/results/genome_annotation/${method}/${brakerset}
+der
+
+##########################################################################
+
+for file in `ls ${pathProteins}/Ensembl103/primary_transcripts/ | grep all.fa`
+do
+    export sp=`echo ${file} | cut -f 1 -d '.'`
+    
+    diamond makedb --in ${pathProteins}/Ensembl103/primary_transcripts/${file} --db ${pathProteins}/Ensembl103/primary_transcripts/${sp}
+done 
+
+##########################################################################
