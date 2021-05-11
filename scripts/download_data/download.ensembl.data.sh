5e0425ab93b60db8790a4988c21b01db0c03903d
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-05-11 21:00:14 +0200

    download gff and genome files from Ensembl

---
+++ scripts/download_data/download.ensembl.data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export cluster=$1
+
+##########################################################################
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+fi
+
+export pathProteins=${path}/data/protein_sequences/Ensembl103
+
+##########################################################################
+
+for file in `ls ${pathProteins} | grep pep.all.fa`
+do
+    export prefix=`basename ${file} .pep.all.fa`
+    export sp=`echo ${prefix} | cut -f 1 -d '.'`
+    export lowersp=`echo "$sp" | awk '{print tolower($0)}'`
+
+    echo ${file} ${prefix} ${sp} ${lowersp}	   
+done
+
+##########################################################################
+
