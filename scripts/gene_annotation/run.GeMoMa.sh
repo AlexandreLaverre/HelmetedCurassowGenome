30f8b70659caf610d16c6c65a13a8b4acd6393f5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-10-29 17:24:02 +0200

    adapt gemoma to run on cluster

--- scripts/gene_annotation/run.GeMoMa.sh
+++ scripts/gene_annotation/run.GeMoMa.sh
@@ -10,11 +10,14 @@ export threads=$5
 
 if [ ${cluster} = "pbil" ]; then
     export path=/beegfs/data/${USER}/HelmetedCurassowGenome
+    export pathTools=/beegfs/home/${USER}/Tools
+    export version=1.8
 fi
 
 if [ ${cluster} = "cloud" ]; then
     export path=/mnt/mydatalocal/HelmetedCurassowGenome
     export pathTools=/mnt/mydatalocal/Tools
+    export version=1.7.1
 fi
 
 export pathGenomeAssembly=${path}/results/genome_assembly/${assembly}
@@ -46,15 +49,43 @@ fi
 
 #########################################################################
 
-export genomefile=`ls ${pathGenomes} | grep ${ref} | grep fa`
-export annotfile=`ls ${pathAnnotations} | grep ${ref} | grep gff`
+export genomefile=`ls ${pathGenomes} | grep ${ref}\. | grep fa`
+export annotfile=`ls ${pathAnnotations} | grep ${ref}\. | grep gff`
+
+#########################################################################
+
+echo "genome file "${genomefile}
+echo "annot file "${annotfile}
 
 #########################################################################
 
 if [ -e ${pathResults}/final_annotation.gff ]; then
     echo "already done"
 else
-    java -jar ${pathTools}/GeMoMa/GeMoMa-1.7.1.jar CLI GeMoMaPipeline threads=${threads} outdir=${pathResults} GeMoMa.Score=ReAlign AnnotationFinalizer.r=NO o=true t=${pathAssembly} i=${ref} a=${pathAnnotations}/${annotfile}  g=${pathGenomes}/${genomefile} GeMoMa.m=500000 Extractor.f=false GeMoMa.i=10 
+    echo "#!/bin/bash" > ${pathScripts}/bsub_script_gemoma
+
+    if [ ${cluster} = "pbil" ]; then
+	echo "#SBATCH --job-name=gemoma_${ref}" >>  ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --output=${pathScripts}/std_output_GEMOMA_${ref}.txt" >>  ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --error=${pathScripts}/std_error_GEMOMA_${ref}.txt" >> ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --partition=normal" >> ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --mem=12G" >> ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --cpus-per-task=${threads}" >> ${pathScripts}/bsub_script_gemoma
+	echo "#SBATCH --time=24:00:00" >> ${pathScripts}/bsub_script_gemoma
+    fi
+    
+    echo "java -jar ${pathTools}/GeMoMa/GeMoMa-${version}.jar CLI GeMoMaPipeline threads=${threads} outdir=${pathResults} GeMoMa.Score=ReAlign AnnotationFinalizer.r=NO o=true t=${pathAssembly} i=${ref} a=${pathAnnotations}/${annotfile}  g=${pathGenomes}/${genomefile} GeMoMa.m=500000 Extractor.f=false GeMoMa.i=10 " >> ${pathScripts}/bsub_script_gemoma
+
+    if [ ${cluster} = "pbil" ]; then
+	sbatch ${pathScripts}/bsub_script_gemoma
+    fi
+
+    if [ ${cluster} = "cloud" ]; then
+	chmod a+x ${pathScripts}/bsub_script_gemoma
+	${pathScripts}/bsub_script_gemoma
+    fi
+
+    
 fi
 
 #########################################################################
