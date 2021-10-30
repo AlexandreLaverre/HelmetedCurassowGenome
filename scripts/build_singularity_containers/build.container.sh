efbaabb8c0bd4eeffbd400ae961c7c6613406a26
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-10-30 09:38:13 +0200

    build singularity containers

---
+++ scripts/build_singularity_containers/build.container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## sudo singularity build Rsing.simg singularity_recipe
+
+## sudo singularity build basic_ubuntu.simg basic_recipe
+
+## sudo singularity build R.simg R_recipe
+
+sudo singularity build mmseqs_java.simg mmseqs_java_recipe
