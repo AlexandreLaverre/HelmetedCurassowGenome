afbc0bf32c3a1e5c6b6391db35a2b191375d8721
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-01 10:46:22 +0100

    split GFFs

---
+++ scripts/gene_annotation/split.GFF.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export sp=$1
+export ref=$2
+export cluster=$3
+
+#########################################################################
+
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/${USER}/HelmetedCurassowGenome
+fi
+
+if [ ${cluster} = "in2p3" ]; then
+    export path=/sps/biometr/necsulea/HelmetedCurassowGenome
+fi
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+fi
+
+export pathAnnotations=${path}/data/genome_annotations/${source}
+export pathScripts=${path}/scripts/gene_annotation
+
+#########################################################################
+
+if [ -e ${pathAnnotations}/parts ]; then
+    echo "dir output exists"
+else
+    mkdir -p ${pathAnnotations}/parts
+fi
+
+#########################################################################
+
+export annotfile=`ls ${pathAnnotations} | grep ${ref}'\.' | grep gff`
+export prefix=`echo ${annotfile} | cut -f 1 -d '-'`
+
+echo "annotation file ".${annotfile}." prefix "${prefix}
+
+#########################################################################
+
+perl ${pathScripts}/split.GFF.pl --pathGFF=${pathAnnotations}/${annotfile} --maxChrSize=50000000 --dirOutput=${pathAnnotations}/parts --prefixOutput=${prefix}
+
+#########################################################################
