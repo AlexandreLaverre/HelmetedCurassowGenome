01fc7506e67cc036238757d62bdfc3d30a98eea2
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-03 07:38:16 +0100

    changed output path

--- scripts/gene_annotation/filter.GeMoMa.predictions.sh
+++ scripts/gene_annotation/filter.GeMoMa.predictions.sh
@@ -21,6 +21,6 @@ export pathScripts=${path}/scripts/gene_annotation
 
 #########################################################################
 
-perl ${pathScripts}/filter.GeMoMa.predictions.pl --pathAnnotGTF=${pathResults}/filtered_predictions.gtf --pathProteins=${pathResults}/filtered_predictions.faa --pathOrthoGroups=NA --minProteinLength=100 --pathOverlapRepeats=${pathResults}/overlap_repeats.txt --maxFractionRepeats=0.5 --source=GeMoMa --pathOutputGTF=${pathResults}/filtered_predictions_minLength100_maxFractionRepeats0.5.gtf --pathOutputFasta=${pathResults}/final_predictions_minLength100_maxFractionRepeats0.5.faa --pathOutputFullFasta=${pathResults}/filtered_predictions_formatted.faa
+perl ${pathScripts}/filter.GeMoMa.predictions.pl --pathAnnotGTF=${pathResults}/filtered_predictions.gtf --pathProteins=${pathResults}/filtered_predictions.faa --pathOrthoGroups=NA --minProteinLength=100 --pathOverlapRepeats=${pathResults}/overlap_repeats.txt --maxFractionRepeats=0.5 --source=GeMoMa --pathOutputGTF=${pathResults}/filtered_predictions_minLength100_maxFractionRepeats0.5.gtf --pathOutputFasta=${pathResults}/filtered_predictions_minLength100_maxFractionRepeats0.5.faa --pathOutputFullFasta=${pathResults}/filtered_predictions_formatted.faa
 
 #########################################################################
