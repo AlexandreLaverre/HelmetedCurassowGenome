faca0efc8927a0c3d1da356c4e4687c72bcc4b46
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-12 15:36:52 +0100

    updated orthofinder pars

--- scripts/gene_annotation/run.OrthoFinder.sh
+++ scripts/gene_annotation/run.OrthoFinder.sh
@@ -3,6 +3,7 @@
 export cluster=$1
 export annot=$2
 export type=$3
+export threads=$4
 
 export assembly="MEGAHIT_RAGOUT"
 
@@ -19,8 +20,20 @@ export pathResults=${path}/results/genome_annotation/${assembly}/${annot}
 
 ulimit -n 50000
 
+##########################################################################
+
+## to determine homologous gene families for annotation
+
 if [ ${type} = "final" ]; then
-    orthofinder -f ${pathResults}/OrthoFinder_${type} -t 30 -I 2 -M msa 
+    orthofinder -f ${pathResults}/OrthoFinder_${type} -t ${threads} -I 2 -M msa
+fi
+
+##########################################################################
+
+## to roughly cluster protein sequences
+
+if [ ${type} = "filtered" ]; then
+    orthofinder -f ${pathResults}/OrthoFinder_${type} -t ${threads} -I 2
 fi
 
 ##########################################################################
