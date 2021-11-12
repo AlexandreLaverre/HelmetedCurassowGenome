9a0ce155b9ec23e238e91b6ede5e8349af4a6e3d
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-12 10:27:38 +0100

    run on cluster

--- scripts/copy_number_variation/process.reads.sh
+++ scripts/copy_number_variation/process.reads.sh
@@ -2,15 +2,23 @@
 
 export library=$1
 export cluster=$2
+export nthreads=$3
 
 #########################################################################
 
 if [ ${cluster} = "pbil" ]; then
     export path=/beegfs/data/necsulea/HelmetedCurassowGenome
+    export pathTools=/beegfs/home/necsulea/Tools
 fi
 
 if [ ${cluster} = "cloud" ]; then
     export path=/mnt/mydatalocal/HelmetedCurassowGenome
+    export pathTools=/mnt/mydatalocal/Tools
+fi
+
+if [ ${cluster} = "in2p3" ]; then
+    export path=/sps/biometr/necsulea/HelmetedCurassowGenome
+    export pathTools=/sps/biometr/necsulea/Tools
 fi
 
 export pathWGS=${path}/data/WGS
@@ -19,28 +27,66 @@ export pathResults=${path}/results/copy_number_variation
 export pathScripts=${path}/scripts/copy_number_variation
 
 #########################################################################
+
+echo "#!/bin/bash" > ${pathScripts}/bsub_script_process
+
+if [ ${cluster} = "pbil" ]; then
+    echo "#SBATCH --job-name=${library}" >>  ${pathScripts}/bsub_script_process
+    echo "#SBATCH --output=${pathScripts}/std_output_process_${library}.txt" >>  ${pathScripts}/bsub_script_process
+    echo "#SBATCH --error=${pathScripts}/std_error_process_${library}.txt" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --partition=normal" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --mem=12G" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --cpus-per-task=${threads}" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --time=24:00:00" >> ${pathScripts}/bsub_script_process
+fi
+
+if [ ${cluster} = "in2p3" ]; then
+    echo "#SBATCH --job-name=${library}" >>  ${pathScripts}/bsub_script_process
+    echo "#SBATCH --output=${pathScripts}/std_output_process_${library}.txt" >>  ${pathScripts}/bsub_script_process
+    echo "#SBATCH --error=${pathScripts}/std_error_process_${library}.txt" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --ntasks=1" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --cpus-per-task=${threads}" >> ${pathScripts}/bsub_script_process
+    echo "#SBATCH --time=7-00:00:00" >> ${pathScripts}/bsub_script_process
+fi
+
+#########################################################################
+
 # Align the data
 
-bwa mem -R "@RG\tID:id\tSM:sample\tLB:lib" ${pathIndex}/genome_sequence_renamed.fa ${pathWGS}/${library}_R1_001_trimmed.fastq.gz ${pathWGS}/${library}_R2_001_trimmed.fastq.gz \
+echo "bwa mem -t ${nthreads} -R "@RG\tID:id\tSM:sample\tLB:lib" ${pathIndex}/genome_sequence_renamed.fa ${pathWGS}/${library}_R1_001_trimmed.fastq.gz ${pathWGS}/${library}_R2_001_trimmed.fastq.gz \
     | samblaster --excludeDups --addMateTags --maxSplitCount 2 --minNonOverlap 20 \
     | samtools view -S -b - \
-    > ${pathResults}/${library}.bam
+    > ${pathResults}/${library}.bam" >> ${pathScripts}/bsub_script_process
 
 #########################################################################
 # Extract the discordant paired-end alignments.
-samtools view -b -F 1294 ${pathResults}/${library}.bam > ${pathResults}/${library}.discordants.unsorted.bam
+
+echo "samtools view -b -F 1294 ${pathResults}/${library}.bam > ${pathResults}/${library}.discordants.unsorted.bam" >> ${pathScripts}/bsub_script_process
 
 #########################################################################
-
 # Extract the split-read alignments
-samtools view -h ${pathResults}/${library}.bam \
-    | scripts/extractSplitReads_BwaMem -i stdin \
+
+echo "samtools view -h ${pathResults}/${library}.bam \
+    | ${pathTools}/lumpy-sv/scripts/extractSplitReads_BwaMem -i stdin \
     | samtools view -Sb - \
-    > ${pathResults}/${library}.splitters.unsorted.bam
+    > ${pathResults}/${library}.splitters.unsorted.bam" >> ${pathScripts}/bsub_script_process
 
 #########################################################################
 # Sort both alignments
-samtools sort ${pathResults}/${library}.discordants.unsorted.bam ${pathResults}/${library}.discordants
-samtools sort ${pathResults}/${library}.splitters.unsorted.bam ${pathResults}/${library}.splitters
+
+echo "samtools sort ${pathResults}/${library}.discordants.unsorted.bam ${pathResults}/${library}.discordants" >> ${pathScripts}/bsub_script_process
+echo "samtools sort ${pathResults}/${library}.splitters.unsorted.bam ${pathResults}/${library}.splitters" >> ${pathScripts}/bsub_script_process
+
+#########################################################################
+## Run everything
+
+if [ ${cluster} = "pbil" ]||[ ${cluster} = "in2p3" ]; then
+    sbatch ${pathScripts}/bsub_script_process
+fi
+
+if [ ${cluster} = "cloud" ]; then
+    chmod a+x ${pathScripts}/bsub_script_process
+    ${pathScripts}/bsub_script_process
+fi
 
 #########################################################################
