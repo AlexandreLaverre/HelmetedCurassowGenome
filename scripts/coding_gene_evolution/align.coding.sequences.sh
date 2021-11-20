d0a296e9656ba438fa90c587734b91260d233eeb
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-20 18:00:09 +0100

    align CDS with prank

--- scripts/coding_gene_evolution/align.coding.sequences.sh
+++ scripts/coding_gene_evolution/align.coding.sequences.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export cluster=$1
+export start=$1
+export end=$2
+export cluster=$3
 
 ##########################################################################
 
@@ -15,14 +17,49 @@ export pathScripts=${path}/scripts/coding_gene_evolution
 
 ##########################################################################
 
-for file in `ls ${pathResults}/CDS | grep unaln `
+echo "#!/bin/bash" > ${pathScripts}/log/bsub_prank_${start}_${end}
+
+if [ ${cluster} = "pbil" ]; then
+    echo "#SBATCH --job-name=prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --partition=normal" >>${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --output=${pathScripts}/log/std_out_prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --error=${pathScripts}/log/std_err_prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --time=2:00:00" >>  ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --mem=20G" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+fi
+
+
+if [ ${cluster} = "in2p3" ]; then
+    echo "#SBATCH --job-name=prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --output=${pathScripts}/log/std_out_prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --error=${pathScripts}/log/std_err_prank_${start}_${end}" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+    echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/log/bsub_prank_${start}_${end}
+fi
+
+##########################################################################
+
+for file in `ls ${pathResults}/CDS | grep unaln | sed -n "${start},${end}p;${end}q"`
 do
     export prefix=`basename ${file} .unaln.fa`
-    
-    prank -codon -f=fasta -t=${pathResults}/CDS/${prefix}.tree -d=${pathResults}/CDS/${file} -o=${pathResults}/CDS/${prefix}.aln.fa
 
-    prank -convert -f=phylips -d=${pathResults}/CDS/${prefix}.aln.fa -o=${pathResults}/CDS/${prefix}.aln.phy
+    echo ${prefix}
+    
+    echo "prank -codon -once -f=fasta -t=${pathResults}/CDS/${prefix}.tree -d=${pathResults}/CDS/${file} -o=${pathResults}/CDS/${prefix}.aln.fa" >> ${pathScripts}/log/bsub_prank_${start}_${end}
+
+    echo "prank -convert -f=phylips -d=${pathResults}/CDS/${prefix}.aln.fa -o=${pathResults}/CDS/${prefix}.aln.phy"  >> ${pathScripts}/log/bsub_prank_${start}_${end}
     
 done
+
+##########################################################################
+
+if [ ${cluster} = "pbil" ]||[ ${cluster} = "in2p3" ]; then
+    sbatch ${pathScripts}/log/bsub_script_prank_${start}_${end}
+fi
+
+if [ ${cluster} = "cloud" ]; then
+    chmod a+x ${pathScripts}/log/bsub_script_prank_${start}_${end}
+    ${pathScripts}/log/bsub_script_prank_${start}_${end}
+fi
 
 ##########################################################################
