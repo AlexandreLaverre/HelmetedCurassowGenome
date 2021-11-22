47dbb52d69a5a6a8947d7b7acc68017a053f64a5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-22 14:52:37 +0100

    run CNVnator by chr

--- scripts/copy_number_variation/run.CNVnator.sh
+++ scripts/copy_number_variation/run.CNVnator.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export cluster=$1
+set -e
+
+export chr=$1
+export cluster=$2
 
 ####################################################################################
 
@@ -24,30 +27,30 @@ export pathResults=${path}/results/CNVnator
 
 ## Extract read mapping
 
-cnvnator -root ${pathResults}/allsamples.root -tree ${pathAln}/accepted_hits_allsamples.bam
+cnvnator -root ${pathResults}/allsamples_${chr}.root -tree ${pathAln}/accepted_hits_allsamples.bam -chrom ${chr}
 
 ##############################################################
 
 # Generate histogram
 
-cnvnator -root ${pathResults}/allsamples.root -his 1000 -fasta ${pathAssembly}/genome_sequence_renamed.fa
+cnvnator -root ${pathResults}/allsamples_${chr}.root -his 1000 -fasta ${pathAssembly}/genome_sequence_renamed.fa
 
 ##############################################################
 
 # Calculate statistics
 
-cnvnator -root ${pathResults}/allsamples.root -stat 1000
+cnvnator -root ${pathResults}/allsamples_${chr}.root -stat 1000
 
 ##############################################################
 
 # Partition
 
-cnvnator -root ${pathResults}/allsamples.root -partition 1000
+cnvnator -root ${pathResults}/allsamples_${chr}.root -partition 1000
 
 ##############################################################
 
 # Call CNVs
 
-cnvnator -root ${pathResults}/allsamples.root -call 1000
+cnvnator -root ${pathResults}/allsamples_${chr}.root -call 1000
 
 ##############################################################
