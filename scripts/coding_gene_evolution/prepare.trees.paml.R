b2ae71e259e8bfa21fcdd69aa400a195bc1184ce
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-25 18:33:32 +0100

    prepare tree paml, draft

---
+++ scripts/coding_gene_evolution/prepare.trees.paml.R
@@ -0,0 +1,32 @@
+#########################################################################
+
+pathResults="../../results/coding_gene_evolution/"
+
+#########################################################################
+
+library(ape)
+
+#########################################################################
+
+full.tree=read.tree(paste(pathResults, "species_tree_rooted.txt",sep=""))
+full.tree$node.label <- NULL
+
+#########################################################################
+
+files=system(paste("ls ",pathResults, "CDS/",sep=""), intern=T)
+
+#########################################################################
+
+for(file in files){
+  prefix=paste(unlist(strsplit(file, split="\\."))[1:2], collapse=".")
+
+  species=system(paste("grep \">\" ",pathResults,"/CDS/",file,sep=""), intern=T)
+  species=unlist(lapply(species, function(x) substr(x,2,nchar(x))))
+
+  this.tree=keep.tip(full.tree, species)
+
+  write.tree(this.tree,file=paste(pathResults, "CDS/",prefix,".tree",sep=""))
+}
+
+#########################################################################
+
