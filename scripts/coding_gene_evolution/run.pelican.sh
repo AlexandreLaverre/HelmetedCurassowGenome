d38d6b3b0b1119ad379539e3f217ba0e7a32b03f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-11-28 18:22:09 +0100

    added dataset par

--- scripts/coding_gene_evolution/run.pelican.sh
+++ scripts/coding_gene_evolution/run.pelican.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-export cluster=$1
-export threads=$2
+set -e
+
+export dataset=$1
+export cluster=$2
+export threads=$3
 
 ##########################################################################
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/home/ubuntu/data/mydatalocal/HelmetedCurassowGenome
-    export pathTools=/home/ubuntu/data/mydatalocal/Tools
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
+    export pathTools=/ifb/data/mydatalocal/Tools
 fi
 
 export pathResults=${path}/results/coding_gene_evolution/
@@ -20,11 +23,11 @@ export OMP_NUM_THREADS=1
 
 ## annotate tree
 
-singularity exec -B ${path} ${pathTools}/pelican.sif pelican tree-parsimony-annotation --output ${pathResults}/pelican_annotated_tree.txt --tree ${pathResults}/species_tree_nobootstrap.txt --annotation ${pathResults}/phenotype_data.txt
+singularity exec -B ${path} ${pathTools}/pelican.sif pelican tree-parsimony-annotation --output ${pathResults}/pelican_annotated_tree_${dataset}.txt --tree ${pathResults}/species_tree_nobootstrap.txt --annotation ${pathResults}/phenotype_data_${dataset}.txt
 
 ##########################################################################
 
-singularity exec -B ${path} ${pathTools}/pelican.sif pelican scan ---multinomial-filter 0.001 --threads ${threads} --translate --tree ${pathResults}/pelican_annotated_tree.txt --alignment ${pathResults}/data_for_pelican --output ${pathResults}/pelican_output --progress-bar
+singularity exec -B ${path} ${pathTools}/pelican.sif pelican scan --multinomial-filter 0.001 --threads ${threads} --translate --tree ${pathResults}/pelican_annotated_tree_${dataset}.txt --alignment ${pathResults}/data_for_pelican --output ${pathResults}/pelican_output_${dataset} --progress-bar
 
 ##########################################################################
 
