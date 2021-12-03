f0e4fb8d52371533f10ae02131a92b8a96033413
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-12-03 17:12:36 +0100

    changed output file name

--- scripts/data_quality/trim.reads.sh
+++ scripts/data_quality/trim.reads.sh
@@ -28,12 +28,18 @@ if [ ${sp} = "Pauxi_pauxi" ]; then
 
     export pathR1=${pathWGS}/${sample}_R1_001.fastq.gz
     export pathR2=${pathWGS}/${sample}_R2_001.fastq.gz
+
+    export pathR1out=${pathWGS}/${sample}_R1_001_trimmed.fastq.gz
+    export pathR2out=${pathWGS}/${sample}_R2_001_trimmed.fastq.gz
 fi
 
 if [ ${sp} = "Basiliscus_vittatus" ]; then
 
     export pathR1=${pathWGS}/${sample}_R1.fastq.gz
     export pathR2=${pathWGS}/${sample}_R2.fastq.gz
+
+    export pathR1out=${pathWGS}/${sample}_R1_trimmed.fastq.gz
+    export pathR2out=${pathWGS}/${sample}_R2_trimmed.fastq.gz
 
     ## TruSeq
     export adapter1=AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT
@@ -44,6 +50,6 @@ fi
 
 echo ${sample}
 
-cutadapt --minimum-length 50 --trim-n -a ${adapter1} -A ${adapter2} -o ${pathWGS}/${sample}_R1_001_trimmed.fastq.gz -p ${pathWGS}/${sample}_R2_001_trimmed.fastq.gz ${pathR1} ${pathR2}
+cutadapt --minimum-length 50 --trim-n -a ${adapter1} -A ${adapter2} -o ${pathR1out} -p ${pathR2out} ${pathR1} ${pathR2}
 
 ###############################################################
