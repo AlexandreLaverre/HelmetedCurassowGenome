293f7fc2b90e5ea7e9faa171fa014c6d5da71246
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-12-03 10:47:45 +0100

    adapter sequences

--- scripts/data_quality/trim.reads.sh
+++ scripts/data_quality/trim.reads.sh
@@ -21,6 +21,18 @@ export pathWGS=${path}/data/WGS/${sp}
 
 ## This is cutadapt 2.8 with Python 3.8.10
 
+if [ ${sp} = "Pauxi_pauxi" ]; then
+    ## Nextera
+    export adapter1=CTGTCTCTTATACACATCT
+    export adapter2=CTGTCTCTTATACACATCT
+fi
+
+if [ ${sp} = "Basiliscus_vittatus" ]; then
+    ## TruSeq
+    export adapter1=AATGATACGGCGACCACCGAGATCTACACTCTTTCCCTACACGACGCTCTTCCGATCT
+    export adapter2=CAAGCAGAAGACGGCATACGAGATCGGTCTCGGCATTCCTGCTGAACCGCTCTTCCGATCT
+fi
+
 ###############################################################
 
 echo ${sample}
@@ -28,6 +40,6 @@ echo ${sample}
 export pathR1=${pathWGS}/${sample}_R1_001.fastq.gz
 export pathR2=${pathWGS}/${sample}_R2_001.fastq.gz
 
-cutadapt --minimum-length 50 --trim-n -a CTGTCTCTTATACACATCT -A CTGTCTCTTATACACATCT -o ${pathWGS}/${sample}_R1_001_trimmed.fastq.gz -p ${pathWGS}/${sample}_R2_001_trimmed.fastq.gz ${pathR1} ${pathR2} 
+cutadapt --minimum-length 50 --trim-n -a ${adapter1} -A ${adapter2} -o ${pathWGS}/${sample}_R1_001_trimmed.fastq.gz -p ${pathWGS}/${sample}_R2_001_trimmed.fastq.gz ${pathR1} ${pathR2}
 
 ###############################################################
