2d480603fcb04271dbb7c74a8f3690b4fb972ba5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-12-06 13:19:35 +0100

    updated script for other species

--- scripts/scaffold_assembly/run.cactus.sh
+++ scripts/scaffold_assembly/run.cactus.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-export cluster=$1
+export sp=$1
+export cluster=$2
+export nthreads=$3
 
 #########################################################################
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+    export path=/home/ubuntu/data/mydatalocal/HelmetedCurassowGenome
 fi
 
-export pathGenomes=${path}/data/genome_sequences
-export pathAssembly=${path}/results/genome_assembly/MEGAHIT
-export pathResults=${path}/results/genome_assembly/MEGAHIT_RAGOUT
+export pathGenomes=${path}/data/genome_sequences/Ensembl103
+export pathAssembly=${path}/results/genome_assembly/${sp}/MEGAHIT
+export pathResults=${path}/results/genome_assembly/${sp}/MEGAHIT_RAGOUT
 
 #########################################################################
 
@@ -25,18 +27,40 @@ fi
 
 #########################################################################
 
-echo "(Anas_platyrhynchos_platyrhynchos, ((Penelope_pileata, Pauxi_pauxi), Gallus_gallus));" > ${pathResults}/seqFile
-echo "" >>  ${pathResults}/seqFile
-
-for sp in Anas_platyrhynchos_platyrhynchos Penelope_pileata  Gallus_gallus
-do
-    echo "${sp} ${pathGenomes}/${sp}/genome_sm_clean.fa" >> ${pathResults}/seqFile
-done
-
-echo "Pauxi_pauxi ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
+if [ ${sp} = "Pauxi_pauxi"]; then
+    echo "(Anas_platyrhynchos_platyrhynchos, ((Penelope_pileata, Pauxi_pauxi), Gallus_gallus));" > ${pathResults}/seqFile
+    echo "" >>  ${pathResults}/seqFile
+    
+    for sp in Anas_platyrhynchos_platyrhynchos Penelope_pileata  Gallus_gallus
+    do
+	echo "${sp} ${pathGenomes}/${sp}/genome_sm_clean.fa" >> ${pathResults}/seqFile
+    done
+    
+    echo "Pauxi_pauxi ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
+fi
 
 #########################################################################
 
-docker run -v ${path}:/mnt/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 cactus --binariesMode local --workDir ${pathResults}/ ${pathResults}/jobStore ${pathResults}/seqFile ${pathResults}/alignment.hal
+if [ ${sp} = "Basiliscus_vittatus"]; then
+    echo "(Salvator_merianae, ((Basiliscus_vittatus, Anolis_carolinensis), Pseudonaja_textilis));" > ${pathResults}/seqFile
+    echo "" >>  ${pathResults}/seqFile
+    
+    for sp in Salvator_merianae Anolis_carolinensis Pseudonaja_textilis
+    do
+	echo "${sp} ${pathGenomes}/${sp}/genome_sm_clean.fa" >> ${pathResults}/seqFile
+    done
+
+    if [ -e ${pathAssembly}/final.contigs.clean.fa ]; then
+	echo "clean file already there"
+    else
+	perl ${pathScripts}/cleanup.fasta.names.pl --pathInput=${pathAssembly}/final.contigs.fa --pathOutput=${pathAssembly}/final.contigs.clean.fa
+    fi
+    
+    echo "Basiliscus_vittatus ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
+fi
+
+#########################################################################
+
+docker run -v ${path}:/mnt/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 cactus --binariesMode local --workDir ${pathResults}/ --maxServiceJobs 1 --maxCores ${nthreads} --maxMemory 60G ${pathResults}/jobStore ${pathResults}/seqFile ${pathResults}/alignment.hal
 
 #########################################################################
