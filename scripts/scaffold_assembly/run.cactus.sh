797bd3d2598befa1ccf2846ef1fbe71a72912643
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2021-12-20 21:34:39 +0100

    restore cactus

--- scripts/scaffold_assembly/run.cactus.sh
+++ scripts/scaffold_assembly/run.cactus.sh
@@ -3,6 +3,7 @@
 export sp=$1
 export cluster=$2
 export nthreads=$3
+export restore=$4
 
 #########################################################################
 
@@ -28,40 +29,48 @@ fi
 
 #########################################################################
 
-if [ ${sp} = "Pauxi_pauxi" ]; then
-    echo "(Anas_platyrhynchos_platyrhynchos, ((Penelope_pileata, Pauxi_pauxi), Gallus_gallus));" > ${pathResults}/seqFile
-    echo "" >>  ${pathResults}/seqFile
+if [ ${restore} = "false" ]; then
     
-    for sp in Anas_platyrhynchos_platyrhynchos Penelope_pileata  Gallus_gallus
-    do
-	echo "${sp} ${pathGenomes}/${sp}/genome_sm_clean.fa" >> ${pathResults}/seqFile
-    done
-    
-    echo "Pauxi_pauxi ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
-fi
-
-#########################################################################
-
-if [ ${sp} = "Basiliscus_vittatus" ]; then
-    echo "(Salvator_merianae, ((Basiliscus_vittatus, Anolis_carolinensis), Pseudonaja_textilis));" > ${pathResults}/seqFile
-    echo "" >>  ${pathResults}/seqFile
-    
-    for sp in Salvator_merianae Anolis_carolinensis Pseudonaja_textilis
-    do
-	echo "${sp} ${pathGenomes}/${sp}.sm.clean.fa" >> ${pathResults}/seqFile
-    done
-
-    if [ -e ${pathAssembly}/final.contigs.clean.fa ]; then
-	echo "clean file already there"
-    else
-	perl ${pathScripts}/cleanup.fasta.names.pl --pathInput=${pathAssembly}/final.contigs.fa --pathOutput=${pathAssembly}/final.contigs.clean.fa
+    if [ ${sp} = "Pauxi_pauxi" ]; then
+	echo "(Anas_platyrhynchos_platyrhynchos, ((Penelope_pileata, Pauxi_pauxi), Gallus_gallus));" > ${pathResults}/seqFile
+	echo "" >>  ${pathResults}/seqFile
+	
+	for sp in Anas_platyrhynchos_platyrhynchos Penelope_pileata  Gallus_gallus
+	do
+	    echo "${sp} ${pathGenomes}/${sp}/genome_sm_clean.fa" >> ${pathResults}/seqFile
+	done
+	
+	echo "Pauxi_pauxi ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
     fi
     
-    echo "Basiliscus_vittatus ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
+    #########################################################################
+    
+    if [ ${sp} = "Basiliscus_vittatus" ]; then
+	echo "(Salvator_merianae, ((Basiliscus_vittatus, Anolis_carolinensis), Pseudonaja_textilis));" > ${pathResults}/seqFile
+	echo "" >>  ${pathResults}/seqFile
+	
+	for sp in Salvator_merianae Anolis_carolinensis Pseudonaja_textilis
+	do
+	    echo "${sp} ${pathGenomes}/${sp}.sm.clean.fa" >> ${pathResults}/seqFile
+	done
+	
+	if [ -e ${pathAssembly}/final.contigs.clean.fa ]; then
+	    echo "clean file already there"
+	else
+	    perl ${pathScripts}/cleanup.fasta.names.pl --pathInput=${pathAssembly}/final.contigs.fa --pathOutput=${pathAssembly}/final.contigs.clean.fa
+	fi
+	
+	echo "Basiliscus_vittatus ${pathAssembly}/final.contigs.clean.fa"  >> ${pathResults}/seqFile
+    fi
+    
+    #########################################################################
+    
+    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 cactus --binariesMode local --workDir ${pathResults}/ --maxServiceJobs 1 --maxCores ${nthreads} --maxMemory 60G ${pathResults}/jobStore ${pathResults}/seqFile ${pathResults}/alignment.hal
+    
+    #########################################################################
+else
+    ## restore
+    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --restart --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 cactus --binariesMode local --workDir ${pathResults}/ --maxServiceJobs 1 --maxCores ${nthreads} --maxMemory 60G ${pathResults}/jobStore ${pathResults}/seqFile ${pathResults}/alignment.hal
 fi
-
-#########################################################################
-
-docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 cactus --binariesMode local --workDir ${pathResults}/ --maxServiceJobs 1 --maxCores ${nthreads} --maxMemory 60G ${pathResults}/jobStore ${pathResults}/seqFile ${pathResults}/alignment.hal
 
 #########################################################################
