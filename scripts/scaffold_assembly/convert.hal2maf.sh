fa2f88cd93db13503330c4f676264bde50f23818
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-17 20:28:11 +0100

    run hal2maf from docker

--- scripts/scaffold_assembly/convert.hal2maf.sh
+++ scripts/scaffold_assembly/convert.hal2maf.sh
@@ -7,13 +7,22 @@ export nthreads=$3
 #########################################################################
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/home/ubuntu/data/mydatalocal/HelmetedCurassowGenome
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
 fi
 
 export pathHAL=${path}/results/genome_assembly/${sp}/MEGAHIT_RAGOUT
 
 #########################################################################
 
-hal2mafMP.py ${pathHAL}/alignment.hal ${pathHAL}/mafs_by_chr/alignment.maf --numProc ${nthreads} --splitBySequence
+if [ -e ${pathHAL}/mafs_by_chr ]; then
+    echo "dir output exists"
+else
+    mkdir ${pathHAL}/mafs_by_chr
+fi
 
 #########################################################################
+
+docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/alignment.hal ${pathHAL}/mafs_by_chr/alignment.maf --numProc ${nthreads} --splitBySequence
+
+#########################################################################
+
