f0d38c37088c1a0470cf6d6580a2d2a11be988b7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-17 22:36:10 +0100

    changed ragout recipe back to maf

--- scripts/scaffold_assembly/run.ragout.sh
+++ scripts/scaffold_assembly/run.ragout.sh
@@ -7,13 +7,13 @@ export ncores=$3
 #########################################################################
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/home/ubuntu/data/mydatalocal/HelmetedCurassowGenome
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
 fi
 
 export pathResults=${path}/results/genome_assembly/${sp}/MEGAHIT_RAGOUT
 
 #########################################################################
 
-ragout -o ${pathResults} -s hal --refine -t ${ncores} ragout.recipe.${cluster}.${sp}
+ragout -o ${pathResults} -s maf --refine -t ${ncores} ragout.recipe.${cluster}.${sp}
 
 #########################################################################
