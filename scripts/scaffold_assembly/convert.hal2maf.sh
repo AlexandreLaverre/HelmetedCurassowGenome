85b9660e6a0c653f832c4990bfef5890fa99a284
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-17 17:14:30 +0100

    changed path; added pars

--- scripts/scaffold_assembly/convert.hal2maf.sh
+++ scripts/scaffold_assembly/convert.hal2maf.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-export cluster=$1
+export sp=$1
+export cluster=$2
+export nthreads=$3
 
 #########################################################################
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+    export path=/home/ubuntu/data/mydatalocal/HelmetedCurassowGenome
 fi
 
-export pathHAL=${path}/results/genome_assembly/MEGAHIT_RAGOUT
+export pathHAL=${path}/results/genome_assembly/${sp}/MEGAHIT_RAGOUT
 
 #########################################################################
 
-hal2mafMP.py ${pathHAL}/alignment.hal ${pathHAL}/mafs_by_chr/alignment.maf --numProc 12 --splitBySequence 
+hal2mafMP.py ${pathHAL}/alignment.hal ${pathHAL}/mafs_by_chr/alignment.maf --numProc ${nthreads} --splitBySequence
 
 #########################################################################
