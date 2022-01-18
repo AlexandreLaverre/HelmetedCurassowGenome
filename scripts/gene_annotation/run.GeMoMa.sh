f45ff8e3237f129bea0a71501f06d6486312a4f7
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-18 17:46:57 +0100

    modified output dir

--- scripts/gene_annotation/run.GeMoMa.sh
+++ scripts/gene_annotation/run.GeMoMa.sh
@@ -30,7 +30,7 @@ fi
 export pathGenomeAssembly=${path}/results/genome_assembly/${assembly}
 export pathGenomes=${path}/data/genome_sequences/${source}
 export pathAnnotations=${path}/data/genome_annotations/${source}
-export pathResults=${path}/results/genome_annotation/${assembly}/GeMoMa/${ref}
+export pathResults=${path}/results/genome_annotation/${target}/${assembly}/GeMoMa/${ref}
 export pathScripts=${path}/scripts/gene_annotation
 export pathScriptsScaffoldAssembly=${path}/scripts/scaffold_assembly
 
