a80bb5c92c9ac604129d35bf8472d018100530d4
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-21 11:21:10 +0100

    fixed number of CPUs if divided protein sequences

--- scripts/genome_assembly_quality/run.tblastn.sh
+++ scripts/genome_assembly_quality/run.tblastn.sh
@@ -79,11 +79,11 @@ if [ ${threads} = "parts" ]; then
 		echo "#SBATCH --error=${pathScripts}/std_error_tblastn_${ref}.txt" >> ${pathScripts}/bsub_script_tblastn
 		echo "#SBATCH --partition=normal" >> ${pathScripts}/bsub_script_tblastn
 		echo "#SBATCH --mem=5G" >> ${pathScripts}/bsub_script_tblastn
-		echo "#SBATCH --cpus-per-task=${threads}" >> ${pathScripts}/bsub_script_tblastn
+		echo "#SBATCH --cpus-per-task=2" >> ${pathScripts}/bsub_script_tblastn
 		echo "#SBATCH --time=24:00:00" >> ${pathScripts}/bsub_script_tblastn
 	    fi
 	    
-	    echo "tblastn -num_threads ${threads} -query ${pathProteinSequences}/fasta_parts/AllPeptides_Ensembl${ensrelease}_part${i}.fa -db ${pathResults}/${suffix} -out ${pathResults}/tblastn_parts/${refsp}_AllPeptides${ensrelease}_vs_${suffix}_part${i}.tblastn.out -evalue 0.001 -outfmt \"6 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps\"" >> ${pathScripts}/bsub_script_tblastn
+	    echo "tblastn -num_threads 2 -query ${pathProteinSequences}/fasta_parts/AllPeptides_Ensembl${ensrelease}_part${i}.fa -db ${pathResults}/${suffix} -out ${pathResults}/tblastn_parts/${refsp}_AllPeptides${ensrelease}_vs_${suffix}_part${i}.tblastn.out -evalue 0.001 -outfmt \"6 qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore gaps\"" >> ${pathScripts}/bsub_script_tblastn
 
 	    if [ ${cluster} = "pbil" ]; then
 		sbatch ${pathScripts}/bsub_script_tblastn
