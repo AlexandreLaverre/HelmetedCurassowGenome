73628a9123e1647b6327d507d2bb189290781b35
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-02-07 15:43:12 +0100

    combine repeats

--- scripts/repeat_annotation/combine.repeat.annotations.sh
+++ scripts/repeat_annotation/combine.repeat.annotations.sh
@@ -2,7 +2,7 @@
 
 ########################################################################
 
-export method=$1
+export assembly=$1
 export cluster=$2
 
 #########################################################################
@@ -12,24 +12,36 @@ if [ ${cluster} = "pbil" ]; then
 fi
 
 if [ ${cluster} = "cloud" ]; then
-    export path=/mnt/mydatalocal/HelmetedCurassowGenome
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
 fi
 
-
-export pathGenomeAssembly=${path}/results/genome_assembly/${method}
-export pathRepeatMasker=${path}/results/repeats/${method}/RepeatMasker
+export pathGenomeAssembly=${path}/results/genome_assembly/${assembly}
+export pathRepeatMasker=${path}/results/repeats/${assembly}/RepeatMasker
 export pathScripts=${path}/scripts/repeat_annotation
 
 #########################################################################
 
-if [ ${method} = "MEGAHIT" ]; then
+if [ ${assembly} = "MEGAHIT" ]; then
     export prefix=final.contigs
 fi
 
 #########################################################################
 
-if [ ${method} = "MEGAHIT_RAGOUT" ]; then
+if [ ${assembly} = "MEGAHIT_RAGOUT" ]; then
     export prefix=genome_sequence_renamed
+fi
+
+#########################################################################
+
+if [ ${assembly} = "NCBI" ]; then
+    export prefix=${sp}
+fi
+
+########################################################################
+
+if [ ${assembly} = "Ensembl103" ]; then
+    export file=`ls ${pathGenomeSequences} | grep ${sp} | grep fa`
+    export prefix=`basename ${file} .fa`
 fi
 
 #########################################################################
@@ -37,4 +49,3 @@ fi
 perl ${pathScripts}/combine.repeat.annotations.pl --pathAnnotation1=${pathRepeatMasker}/Dfam/${prefix}.fa.out --pathAnnotation2=${pathRepeatMasker}/RepeatModeler/${prefix}.fa.out --pathOutput=${pathRepeatMasker}/CombinedRepeatAnnotations.txt --pathOutputBED=${pathRepeatMasker}/CombinedRepeatAnnotations.bed
 
 #########################################################################
-
