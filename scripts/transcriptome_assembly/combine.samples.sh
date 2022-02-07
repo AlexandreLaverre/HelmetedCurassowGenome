622753ea2c5de0d957c41f57f1ff3a42d41dab2a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-02-07 12:46:03 +0100

    combine samples for transcriptome assembly

---
+++ scripts/transcriptome_assembly/combine.samples.sh
@@ -0,0 +1,43 @@
+!/bin/bash
+
+export sp=$1
+export trimmed=$2
+export cluster=$3
+
+#############################################################################
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
+fi
+
+export pathData=${path}/data/RNASeq/${sp}
+export pathResults=${path}/results/transcriptome_assembly/${sp}/TransLiG
+
+## TransLiG v1.3
+
+#############################################################################
+
+if [ -e ${pathResults} ]; then
+    echo "path output exists"
+else
+    mkdir -p ${pathResults}
+fi
+
+#############################################################################
+
+## first combine all samples, paired_end only
+
+if [ -e ${pathData}/allsamples_R1.fastq.gz ]||[ -e ${pathData}/allsamples_R2.fastq.gz ]; then
+    echo "combined path exists"
+else
+    echo "combining samples"
+    if [ ${trimmed} = "true" ]; then
+	cat ${pathData}/*_R1_trimmed.fastq.gz > ${pathData}/allsamples_R1.fastq.gz
+	cat ${pathData}/*_R2_trimmed.fastq.gz > ${pathData}/allsamples_R2.fastq.gz
+    else
+	cat ${pathData}/*_R1.fastq.gz > ${pathData}/allsamples_R1.fastq.gz
+	cat ${pathData}/*_R2.fastq.gz > ${pathData}/allsamples_R2.fastq.gz
+    fi
+fi
+
+#############################################################################
