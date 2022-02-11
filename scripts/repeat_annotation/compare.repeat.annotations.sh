a54b704f3a653d2db7cd7e65d785d495cbd19490
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-02-11 15:44:11 +0100

    use .gz files

--- scripts/repeat_annotation/compare.repeat.annotations.sh
+++ scripts/repeat_annotation/compare.repeat.annotations.sh
@@ -42,8 +42,20 @@ fi
 ########################################################################
 
 if [ ${assembly} = "Ensembl103" ]; then
-    export file=`ls ${pathGenomeSequences} | grep ${sp} | grep fa`
-    export prefix=`basename ${file} .fa`
+    export file=`ls ${pathGenomeSequences} | grep ${sp} | grep fa.gz | grep -v sm`
+
+    if [ -e ${pathGenomeSequences}/${file} ]; then
+	export prefix=`basename ${file} .fa.gz`
+    else
+	export file=`ls ${pathGenomeSequences} | grep ${sp} | grep fa | grep -v sm`
+
+	if [ -e ${pathGenomeSequences}/${file} ]; then
+	    export prefix=`basename ${file} .fa`
+	else
+	    echo "cannot find genome sequence file"
+	    exit
+	fi
+    fi
 fi
 
 #########################################################################
