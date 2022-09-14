472a93b554b5611f2b8598e7edb4f1ca28b6f5e5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-09-14 17:34:41 +0200

    run rename transcripts for all species

--- scripts/filter_gene_annotation/rename.GeMoMa.transcripts.sh
+++ scripts/filter_gene_annotation/rename.GeMoMa.transcripts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export assembly=$1
-export cluster=$2
+export sp=$1
+export assembly=$2
+export cluster=$3
 
 #########################################################################
 
@@ -13,11 +14,13 @@ if [ ${cluster} = "cloud" ]; then
     export path=/mnt/mydatalocal/HelmetedCurassowGenome
 fi
 
-export pathResults=${path}/results/genome_annotation/${assembly}/GeMoMa/combined
-export pathScripts=${path}/scripts/gene_annotation
+export pathResults=${path}/results/genome_annotation/${sp}/${assembly}/GeMoMa/combined
+export pathScripts=${path}/scripts/filter_gene_annotation
 
 #########################################################################
 
 perl ${pathScripts}/rename.GeMoMa.transcripts.pl --pathInputGTF=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.5.gtf --pathOutputGTF=${pathResults}/final_annotations.gtf
+
+cp ${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.5.faa ${pathResults}/final_annotations.faa
 
 #########################################################################
