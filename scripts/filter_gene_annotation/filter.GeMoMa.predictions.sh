2e832ff78855484541b313c2d713b539d7a356cb
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-09-15 10:05:55 +0200

    run filter with different parameters

--- scripts/filter_gene_annotation/filter.GeMoMa.predictions.sh
+++ scripts/filter_gene_annotation/filter.GeMoMa.predictions.sh
@@ -14,10 +14,29 @@ if [ ${cluster} = "cloud" ]; then
     export path=/mnt/mydatalocal/HelmetedCurassowGenome
 fi
 
-
-export pathGenomeAssembly=${path}/results/genome_assembly/${sp}/${assembly}
 export pathResults=${path}/results/genome_annotation/${sp}/${assembly}/GeMoMa/combined
 export pathScripts=${path}/scripts/filter_gene_annotation
+
+#########################################################################
+
+if [ ${assembly} = "MEGAHIT" ]; then
+    export pathGenomeAssembly=${path}/results/genome_assembly/${sp}/${assembly}
+    export pathAssembly=${pathGenomeAssembly}/final.contigs.fa
+fi
+
+#########################################################################
+
+if [ ${assembly} = "MEGAHIT_RAGOUT" ]; then
+    export pathGenomeAssembly=${path}/results/genome_assembly/${sp}/${assembly}
+    export pathAssembly=${pathGenomeAssembly}/genome_sequence_renamed_sm.fa
+fi
+
+#########################################################################
+
+if [ ${assembly} = "NCBI" ]; then
+    export pathGenomeSequences=${path}/data/genome_sequences/${assembly}
+    export pathAssembly=${pathGenomeSequences}/${sp}.fa
+fi
 
 #########################################################################
 
@@ -25,8 +44,17 @@ export pathDiamondResults=${pathResults}/diamond_results/SignificantHits_MinProt
 
 #########################################################################
 
-## quantile(chicken protein lengths, p=0.01) = 68 
+## quantile(chicken protein lengths, p=0.01) = 68
 
-perl ${pathScripts}/filter.GeMoMa.predictions.pl --pathAnnotGTF=${pathResults}/filtered_predictions.gtf --pathProteins=${pathResults}/filtered_predictions.faa --pathDiamondResults=${pathDiamondResults} --minProteinLength=70 --pathOverlapRepeats=${pathResults}/overlap_repeats.txt --maxFractionRepeats=0.25 --source=GeMoMa --pathOutputGTF=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.25.gtf --pathOutputFasta=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.25.faa 
+for minlen in 70 100
+do
+    for maxrep in 0.25 0.5
+    do
 
+	perl ${pathScripts}/filter.GeMoMa.predictions.pl --pathAnnotGTF=${pathResults}/filtered_predictions.gtf --pathProteins=${pathResults}/filtered_predictions.faa --pathDiamondResults=${pathDiamondResults} --minProteinLength=${minlen} --pathOverlapRepeats=${pathResults}/overlap_repeats.txt --maxFractionRepeats=${maxrep} --source=GeMoMa --pathOutputGTF=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength${minlen}_maxFractionRepeats${maxrep}.gtf --pathOutputFasta=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength${minlen}_maxFractionRepeats${maxrep}.faa
+
+	gffread -S -x ${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength${minlen}_maxFractionRepeats${maxrep}.cds.fa -g ${pathAssembly} ${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength${minlen}_maxFractionRepeats${maxrep}.gtf
+	
+    done
+done
 #########################################################################
