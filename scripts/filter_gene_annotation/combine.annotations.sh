4d495a22f5a3e0878d176f413a69eacc3ccc3b94
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-09-19 15:36:29 +0200

    extract fasta sequences

--- scripts/filter_gene_annotation/combine.annotations.sh
+++ scripts/filter_gene_annotation/combine.annotations.sh
@@ -18,6 +18,20 @@ export pathAnnot=${path}/data/genome_annotations/${assembly}
 export pathResults=${path}/results/genome_annotation/${sp}/${assembly}/GeMoMa/combined
 export pathScripts=${path}/scripts/filter_gene_annotation
 
+############################################################################
+
+if [ ${assembly} = "MEGAHIT_RAGOUT" ]; then
+    export pathGenomeAssembly=${path}/results/genome_assembly/${sp}/${assembly}
+    export pathAssembly=${pathGenomeAssembly}/genome_sequence_renamed_sm.fa
+fi
+
+#############################################################################
+
+if [ ${assembly} = "NCBI" ]; then
+    export pathGenomeSequences=${path}/data/genome_sequences/${assembly}
+    export pathAssembly=${pathGenomeSequences}/${sp}.fa
+fi
+
 #############################################################################
 
 if [ -e ${pathAnnot}/${sp}.gtf ]; then
@@ -33,5 +47,10 @@ perl ${pathScripts}/select.new.transcripts.pl --pathAnnot1=${pathAnnot}/${sp}.gt
 #############################################################################
 
 perl ${pathScripts}/combine.annotations.pl --pathAnnot1=${pathAnnot}/${sp}.gtf --pathAnnot2=${pathResults}/filtered_GeMoMa_annotations.gtf  --chrList=NA --pathSelectedTranscripts=${pathResults}/selected_transcripts_vs_${assembly}.txt --pathOutputGTF=${pathResults}/combined_annotations_${assembly}_GeMoMa.gtf
+
+#############################################################################
+
+gffread -S -x ${pathResults}/combined_annotations_${assembly}_GeMoMa.cds.fa -g ${pathAssembly} ${pathResults}/combined_annotations_${assembly}_GeMoMa.gtf
+gffread -S -y ${pathResults}/combined_annotations_${assembly}_GeMoMa.faa -g ${pathAssembly} ${pathResults}/combined_annotations_${assembly}_GeMoMa.gtf
 
 #############################################################################
--- scripts/filter_gene_annotation/rename.GeMoMa.transcripts.sh
+++ scripts/filter_gene_annotation/rename.GeMoMa.transcripts.sh
@@ -21,4 +21,8 @@ export pathScripts=${path}/scripts/filter_gene_annotation
 
 perl ${pathScripts}/rename.GeMoMa.transcripts.pl --pathInputGTF=${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.25_maxXStop0.1.gtf --pathOutputGTF=${pathResults}/filtered_GeMoMa_annotations.gtf
 
+cp ${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.25_maxXStop0.1.cds.fa ${pathResults}/filtered_GeMoMa_annotations.cds.fa
+
+cp ${pathResults}/filtered_predictions_minDiamondProteinFraction0.25_minLength70_maxFractionRepeats0.25_maxXStop0.1.faa ${pathResults}/filtered_GeMoMa_annotations.faa
+
 #########################################################################
