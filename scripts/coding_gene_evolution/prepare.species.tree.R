f7f0acfc167a82cdaefefbba4f308cecf6a46d48
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-03 21:21:17 +0100

    new outgroup

--- scripts/coding_gene_evolution/prepare.species.tree.R
+++ scripts/coding_gene_evolution/prepare.species.tree.R
@@ -4,13 +4,14 @@ library(ape)
 
 ########################################################################
 
+pathOrthoFinder="../../results/gene_families/OrthoFinder/iqtree/Results_Sep21/"
 pathResults="../../results/coding_gene_evolution/"
 
 ########################################################################
 
-tree=read.tree(paste(pathResults, "OrthoFinder_fasttree/Results_Nov16/Species_Tree/SpeciesTree_rooted.txt",sep=""))
+tree=read.tree(paste(pathOrthoFinder, "/Species_Tree/SpeciesTree_rooted.txt",sep=""))
 
-rooted=root(tree, outgroup=c("Struthio_camelus_australis", "Casuarius_casuarius", "Dromaius_novaehollandiae"))
+rooted=root(tree, outgroup=c("Salvator_merianae", "Podarcis_muralis", "Pogona_vitticeps", "Basiliscus_vittatus", "Anolis_carolinensis", "Naja_naja", "Notechis_scutatus", "Pseudonaja_textilis"))
 
 write.tree(rooted, file=paste(pathResults, "species_tree.txt",sep=""))
 
