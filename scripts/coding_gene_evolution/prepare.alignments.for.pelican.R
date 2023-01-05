6a876281ee09d5024536ebcbb4231d99c0e1081c
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-05 09:19:31 +0100

    run on several datasets

--- scripts/coding_gene_evolution/prepare.alignments.for.pelican.R
+++ scripts/coding_gene_evolution/prepare.alignments.for.pelican.R
@@ -1,55 +1,75 @@
 ###########################################################################
-
-pathResults="../../results/coding_gene_evolution"
-pathOrthoGroups=paste(pathResults, "/OrthoFinder_iqtree/Results_Nov17/Phylogenetic_Hierarchical_Orthogroups/",sep="")
-pathCDS=paste(pathResults, "/CDS/",sep="")
-pathOutput=paste(pathResults, "/data_for_pelican/",sep="")
-pathAnnot="../../results/genome_annotation/MEGAHIT_RAGOUT/GeMoMa/combined/"
-
 ###########################################################################
 
 library(stringr)
 
+pathOrthoGroups="../../results/gene_families/OrthoFinder/iqtree/Results_Sep21/Phylogenetic_Hierarchical_Orthogroups/"
+pathAnnot="../../data/ensembl_annotations/Chicken/"
 ###########################################################################
 
-names=read.table(paste(pathAnnot, "homology_inferred_gene_names.txt",sep=""), h=T, stringsAsFactors=F)
-rownames(names)=names$GeneID
+for(dataset in c("birds", "all_species")){
 
-###########################################################################
+    pathResults=paste("../../results/coding_gene_evolution/",dataset,sep="")
 
-N0=read.table(paste(pathOrthoGroups, "N0.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
+    pathCDS=paste(pathResults, "/CDS/",sep="")
+    pathOutput=paste(pathResults, "/data_for_pelican/",sep="")
 
-###########################################################################
+   ###########################################################################
 
-for(i in 1:nrow(N0)){
-  hog=N0$HOG[i]
-  og=N0$OG[i]
-
-  pathIn=paste(pathCDS, hog,"_",og,".aln.best.fas",sep="")
-  
-  if(file.exists(pathIn)){
-    id.pauxi=N0[i,"Pauxi_pauxi"]
-
-    if(id.pauxi%in%rownames(names)){
-      name.pauxi=names[id.pauxi, "GeneName.Pauxi_pauxi"]
-
-      if(length(grep("/",name.pauxi))!=0){
-        name.pauxi=str_replace(name.pauxi, '/', '_')
-      }
-      
-      if(!is.na(name.pauxi)){
-        system(paste("cp ",pathIn, " ",pathOutput, "/",id.pauxi,"_",name.pauxi,".fa",sep=""))
-      } else{
-        system(paste("cp ",pathIn, " ",pathOutput, "/",id.pauxi,".fa",sep=""))
-      }
+    if(dir.exists(pathOutput)){
+        print("path output already there")
     } else{
-      system(paste("cp ",pathIn, " ",pathOutput, "/",id.pauxi,".fa",sep=""))
+        system(paste("mkdr ",pathOutput))
     }
-  }
 
-  if(i%%1000==0){
-    print(i)
-  }
+    ###########################################################################
+
+    names=read.table(paste(pathAnnot, "GeneNames_Ensembl103.txt", sep=""), h=T, stringsAsFactors=F)
+    rownames(names)=names[,1]
+
+    ###########################################################################
+
+    if(dataset == "all_species"){
+        orthogroups=read.table(paste(pathOrthoGroups, "N0.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
+    }
+
+    if(dataset == "birds"){
+        orthogroups=read.table(paste(pathOrthoGroups, "N2.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
+    }
+
+    ###########################################################################
+
+    for(i in 1:nrow(orthogroups)){
+        hog=orthogroups$HOG[i]
+        og=orthogroups$OG[i]
+
+        pathIn=paste(pathCDS, hog,"_",og,".aln.best.fas",sep="")
+
+        if(file.exists(pathIn)){
+            id.chicken=orthogroups[i,"Gallus_gallus"]
+            id.chicken=unlist(strsplit(".", id.chicken))[1]
+
+            if(id.chicken%in%rownames(names)){
+                name.chicken=names[id.chicken, 2]
+
+                if(length(grep("/",name.chicken))!=0){
+                    name.chicken=str_replace(name.chicken, '/', '_')
+                }
+
+                if(!is.na(name.chicken) & name.chicken!=""){
+                    system(paste("cp ",pathIn, " ",pathOutput, "/",id.chicken,"_",name.chicken,".fa",sep=""))
+                } else{
+                    system(paste("cp ",pathIn, " ",pathOutput, "/",id.chicken,".fa",sep=""))
+                }
+            } else{
+                system(paste("cp ",pathIn, " ",pathOutput, "/",hog,"_",og,".fa",sep=""))
+            }
+        }
+
+        if(i%%1000==0){
+            print(i)
+        }
+    }
 }
 
 ###########################################################################
