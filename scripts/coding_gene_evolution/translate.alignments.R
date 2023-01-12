5ec3e020179439b45b5a7605f744bef5cc1561fc
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-12 22:12:16 +0100

    translate alignments for all species

--- scripts/coding_gene_evolution/translate.alignments.R
+++ scripts/coding_gene_evolution/translate.alignments.R
@@ -4,14 +4,6 @@ library(seqinr)
 
 ########################################################################
 
-pathResults="../../results/coding_gene_evolution/"
-
-########################################################################
-
-files=system(paste("ls ",pathResults, "data_for_pelican/", sep=""), intern=T)
-
-########################################################################
-
 translate_alignment <- function(seq){
   startpos=seq(from=1, to=length(seq), by=3)
 
@@ -22,25 +14,39 @@ translate_alignment <- function(seq){
 
 ########################################################################
 
-results=read.table(paste(pathResults, "pelican_output_by_category/best_sites.tsv",sep=""),h=T, stringsAsFactors=F)
+for(spset in c("all_species", "without_chameleons")){
+  for(dataset in c("all_species", "birds", "squamates")){
+    
+    pathResults=paste("../../results/coding_gene_evolution/",spset, "/", dataset, "/",sep="")
 
-########################################################################
-
-nbdone=0
-
-for(gene in unique(results$alignment)){
-  file=paste(gene, ".fa", sep="")
-  
-  aln=read.fasta(paste(pathResults,"data_for_pelican/",file,sep=""))
-
-  proteins=lapply(aln, translate_alignment)
-
-  write.fasta(proteins, names=names(proteins), file.out=paste(pathResults,"translated_alignments/",file,sep=""))
-  
-  nbdone=nbdone+1
-
-  if(nbdone%%100==0){
-    print(nbdone)
+    if(dir.exists(paste(pathResults,"translated_alignments/",sep=""))){
+      print("path output already there")
+    } else{
+      system(paste("mkdir ",pathResults,"translated_alignments/",sep=""))
+    }
+    
+    ########################################################################
+    
+    files=system(paste("ls ",pathResults, "CDS/ | grep aln.best.fas", sep=""), intern=T)
+    
+    ########################################################################
+    
+    nbdone=0
+    
+    for(file in files){
+            
+      aln=read.fasta(paste(pathResults,"CDS/",file,sep=""))
+      
+      proteins=lapply(aln, translate_alignment)
+      
+      write.fasta(proteins, names=names(proteins), file.out=paste(pathResults,"translated_alignments/",file,sep=""))
+      
+      nbdone=nbdone+1
+      
+      if(nbdone%%100==0){
+        print(nbdone)
+      }
+    }
   }
 }
 
