e32b62227a64674d4aed3d1aae5237c3afce04d0
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-12 14:37:35 +0100

    added new sets of species in pelican

--- scripts/coding_gene_evolution/prepare.phenotypes.for.pelican.R
+++ scripts/coding_gene_evolution/prepare.phenotypes.for.pelican.R
@@ -1,52 +1,61 @@
 ###########################################################################
 
-pathResults="../../results/coding_gene_evolution"
-pathOrthoGroups=paste(pathResults, "/OrthoFinder_iqtree/Results_Nov17/Phylogenetic_Hierarchical_Orthogroups/",sep="")
-pathCDS=paste(pathResults, "/CDS/",sep="")
-pathOutput=paste(pathResults, "/data_for_pelican/",sep="")
-pathAnnot="../../results/genome_annotation/MEGAHIT_RAGOUT/GeMoMa/combined/"
-
-###########################################################################
-
 library(ape)
 
 ###########################################################################
 
-full.tree=read.tree(paste(pathResults, "/species_tree_rooted.txt",sep=""))
+all=c("Struthio_camelus_australis", "Casuarius_casuarius", "Dromaius_novaehollandiae", "Gallus_gallus", "Meleagris_gallopavo", "Numida_meleagris", "Penelope_pileata", "Alectura_lathami", "Anseranas_semipalmata", "Anas_platyrhynchos_platyrhynchos", "Anser_cygnoides", "Balearica_regulorum", "Grus_americana", "Calidris_pugnax", "Bucorvus_abyssinicus", "Buceros_rhinoceros", "Upupa_epops", "Rhinopomastus_cyanomelas", "Strix_occidentalis_caurina", "Ficedula_albicollis", "Anser_brachyrhynchus", "Aquila_chrysaetos_chrysaetos", "Geospiza_fortis", "Parus_major", "Serinus_canaria", "Strigops_habroptila", "Coturnix_japonica", "Anolis_carolinensis", "Podarcis_muralis", "Pogona_vitticeps", "Pseudonaja_textilis", "Salvator_merianae", "Naja_naja", "Notechis_scutatus", "Chamaeleo_chamaeleon_recticrista", "Chamaeleo_calyptratus", "Basiliscus_vittatus", "Pauxi_pauxi")
 
-if(file.exists(paste(pathResults, "/species_tree_nobootstrap.txt",sep=""))){
-  print("tree already there")
-}  else {
-  full.tree$node.label <- NULL
-  write.tree(full.tree, paste(pathResults, "/species_tree_nobootstrap.txt",sep=""))
-}
+withoutchameleons=c("Struthio_camelus_australis", "Casuarius_casuarius", "Dromaius_novaehollandiae", "Gallus_gallus", "Meleagris_gallopavo", "Numida_meleagris", "Penelope_pileata", "Alectura_lathami", "Anseranas_semipalmata", "Anas_platyrhynchos_platyrhynchos", "Anser_cygnoides", "Balearica_regulorum", "Grus_americana", "Calidris_pugnax", "Bucorvus_abyssinicus", "Buceros_rhinoceros", "Upupa_epops", "Rhinopomastus_cyanomelas", "Strix_occidentalis_caurina", "Ficedula_albicollis", "Anser_brachyrhynchus", "Aquila_chrysaetos_chrysaetos", "Geospiza_fortis", "Parus_major", "Serinus_canaria", "Strigops_habroptila", "Coturnix_japonica", "Anolis_carolinensis", "Podarcis_muralis", "Pogona_vitticeps", "Pseudonaja_textilis", "Salvator_merianae", "Naja_naja", "Notechis_scutatus",  "Basiliscus_vittatus", "Pauxi_pauxi")
+
+birds=c("Struthio_camelus_australis", "Casuarius_casuarius", "Dromaius_novaehollandiae", "Gallus_gallus", "Meleagris_gallopavo", "Numida_meleagris", "Penelope_pileata", "Alectura_lathami", "Anseranas_semipalmata", "Anas_platyrhynchos_platyrhynchos", "Anser_cygnoides", "Balearica_regulorum", "Grus_americana", "Calidris_pugnax", "Bucorvus_abyssinicus", "Buceros_rhinoceros", "Upupa_epops", "Rhinopomastus_cyanomelas", "Strix_occidentalis_caurina", "Ficedula_albicollis", "Anser_brachyrhynchus", "Aquila_chrysaetos_chrysaetos", "Geospiza_fortis", "Parus_major", "Serinus_canaria", "Strigops_habroptila", "Coturnix_japonica",  "Pauxi_pauxi")
 
 ###########################################################################
 
-for(dataset in c("all_species", "by_category")){
+protuberance=c("Anseranas_semipalmata", "Anser_cygnoides", "Numida_meleagris", "Casuarius_casuarius", "Balearica_regulorum", "Bucorvus_abyssinicus", "Buceros_rhinoceros", "Pauxi_pauxi", "Basiliscus_vittatus", "Chamaeleo_calyptratus")
 
-  helmeted=c("Anseranas_semipalmata", "Numida_meleagris", "Casuarius_casuarius", "Balearica_regulorum", "Bucorvus_abyssinicus", "Buceros_rhinoceros", "Pauxi_pauxi")
+upper_beak=c("Bucorvus_abyssinicus", "Buceros_rhinoceros", "Pauxi_pauxi")
+dorsal_neurocranium=c("Numida_meleagris", "Casuarius_casuarius")
+frontal_area=c("Anseranas_semipalmata", "Anser_cygnoides", "Balearica_regulorum")
+dorsal_area=c("Basiliscus_vittatus", "Chamaeleo_calyptratus")
 
-  upper_beak=c("Bucorvus_abyssinicus", "Buceros_rhinoceros", "Pauxi_pauxi")
-  dorsal_neurocranium=c("Numida_meleagris", "Casuarius_casuarius")
-  frontal_area=c("Anseranas_semipalmata", "Balearica_regulorum")
+###########################################################################
 
-  all.species=full.tree$tip.label
-  
-  phenotype.data=data.frame("species"=all.species, "trait"=rep(NA, length(all.species)))
-  phenotype.data$trait[which(!(phenotype.data$species%in%helmeted))]="non-helmeted"
+for(spset in c("all_species", "without_chameleons")){
+    for(dataset in c("all_species", "birds")){
 
-  if(dataset == "all_species"){
-    phenotype.data$trait[which(phenotype.data$species%in%helmeted)]="helmeted"
-  }
+        pathResults=paste("../../results/coding_gene_evolution/",spset, "/",dataset, sep="")
 
-  if(dataset == "by_category"){
-    phenotype.data$trait[which(phenotype.data$species%in%upper_beak)]="upper_beak"
-    phenotype.data$trait[which(phenotype.data$species%in%dorsal_neurocranium)]="dorsal_neurocranium"
-    phenotype.data$trait[which(phenotype.data$species%in%frontal_area)]="frontal_area"
-  }
-  
-  write.table(phenotype.data, file=paste(pathResults, "/phenotype_data_",dataset,".txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+        full.tree=read.tree(paste(pathResults, "/species_tree.txt",sep=""))
+
+        if(file.exists(paste(pathResults, "/species_tree_nobootstrap.txt",sep=""))){
+            print("tree already there")
+        }  else {
+            full.tree$node.label <- NULL
+            write.tree(full.tree, paste(pathResults, "/species_tree_nobootstrap.txt",sep=""))
+        }
+
+        for(phenotype in c("all_species", "by_category")){
+
+            all.species=full.tree$tip.label
+
+            phenotype.data=data.frame("species"=all.species, "trait"=rep(NA, length(all.species)))
+            phenotype.data$trait[which(!(phenotype.data$species%in%helmeted))]="non-helmeted"
+
+            if(phenotype == "general"){
+                phenotype.data$trait[which(phenotype.data$species%in%helmeted)]="helmeted"
+            }
+
+            if(phenotype == "by_category"){
+                phenotype.data$trait[which(phenotype.data$species%in%upper_beak)]="upper_beak"
+                phenotype.data$trait[which(phenotype.data$species%in%dorsal_neurocranium)]="dorsal_neurocranium"
+                phenotype.data$trait[which(phenotype.data$species%in%frontal_area)]="frontal_area"
+                phenotype.data$trait[which(phenotype.data$species%in%dorsal_area)]="dorsal_area"
+            }
+
+            write.table(phenotype.data, file=paste(pathResults, "/phenotype_data_",phenotype,".txt",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+        }
+    }
 }
 
 ###########################################################################
--- scripts/coding_gene_evolution/run.pelican.sh
+++ scripts/coding_gene_evolution/run.pelican.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-export dataset=$1
-export cluster=$2
-export threads=$3
+export spset=$1
+export dataset=$2
+export phenannot=$3
+export cluster=$4
+export threads=$5
 
 ##########################################################################
 
@@ -13,7 +15,12 @@ if [ ${cluster} = "cloud" ]; then
     export pathTools=/ifb/data/mydatalocal/Tools
 fi
 
-export pathResults=${path}/results/coding_gene_evolution/
+if [ ${cluster} = "pbil" ]; then
+    export path=/beegfs/data/necsulea/HelmetedCurassowGenome
+    export pathTools=/beegfs/home/necsulea/Tools
+fi
+
+export pathResults=${path}/results/coding_gene_evolution/${spset}/${dataset}
 
 ##########################################################################
 
@@ -23,11 +30,11 @@ export OMP_NUM_THREADS=1
 
 ## annotate tree
 
-singularity exec -B ${path} ${pathTools}/pelican.sif pelican tree-parsimony-annotation --output ${pathResults}/pelican_annotated_tree_${dataset}.txt --tree ${pathResults}/species_tree_nobootstrap.txt --annotation ${pathResults}/phenotype_data_${dataset}.txt
+singularity exec -B ${path} ${pathTools}/pelican.sif pelican tree-parsimony-annotation --output ${pathResults}/pelican_annotated_tree_${phenannot}.txt --tree ${pathResults}/species_tree_nobootstrap.txt --annotation ${pathResults}/phenotype_data_${phenannot}.txt
 
 ##########################################################################
 
-singularity exec -B ${path} ${pathTools}/pelican.sif pelican scan --multinomial-filter 0.001 --threads ${threads} --translate --tree ${pathResults}/pelican_annotated_tree_${dataset}.txt --alignment ${pathResults}/data_for_pelican --output ${pathResults}/pelican_output_${dataset} --progress-bar
+singularity exec -B ${path} ${pathTools}/pelican.sif pelican scan --multinomial-filter 0.001 --threads ${threads} --translate --tree ${pathResults}/pelican_annotated_tree_${phenannot}.txt --alignment ${pathResults}/data_for_pelican --output ${pathResults}/pelican_output_${phenannot} --progress-bar
 
 ##########################################################################
 
