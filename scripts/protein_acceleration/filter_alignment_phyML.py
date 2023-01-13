abbf10542251994d3f773607e283c33280de1828
Alexandre <alexandre.laverre@unil.ch>
2023-01-13 16:22:36 +0100

    Adapt phyML pipeline

---
+++ scripts/protein_acceleration/filter_alignment_phyML.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import sys
+from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+
+###################################################################################################
+
+PathAlign = sys.argv[1]
+IDList = sys.argv[2]
+PathResults = sys.argv[3]
+minLength = sys.argv[4]
+
+Part = IDList.split("/")[-1].strip(".txt")
+pathFiltered = PathResults + "/FilteredAlign/" + Part + "/"
+
+###################################################################################################
+
+with open(IDList, 'r') as f:
+    for ID in f.readlines():
+            ID = ID.strip("\n")
+            ids = []
+            seqs = []
+
+            align = AlignIO.read(PathAlign + ID + ".aln.best.fas", "fasta")
+            align_length = align.get_alignment_length()
+
+            # Keep sequences with low number of GAP
+            for seq in align:
+                if seq.count("-")/align_length < float(minLength):
+                    ids.append(id)
+                    seqs.append(seq)
+
+            records = (SeqRecord(Seq(s), id=i) for (i, s) in zip(ids, seqs))
+
+            if len(ids) > 1:
+                with open(pathFiltered + ID + ".rphylip", "w") as OutFile:
+                    AlignIO.write(MultipleSeqAlignment(records), OutFile, "phylip-relaxed")
+
+###################################################################################################
