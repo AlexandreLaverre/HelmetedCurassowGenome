269dce5b3313a91aa9e5ae114b8a408fa521963b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-15 21:39:31 +0100

    extract alignments with hal2maf

---
+++ scripts/detect_conserved_elements/extract.alignments.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## original script by Alexandre Laverré & Anamaria Necsulea
+
+export dataset=$1
+export chr=$2
+export start=$3
+export length=$4
+export cluster=$5
+export nthreads=$6
+
+#########################################################################
+
+if [ ${cluster} = "cloud" ]; then
+    export path=/ifb/data/mydatalocal/HelmetedCurassowGenome
+fi
+
+export pathHAL=${path}/results/whole_genome_alignments/avian_366
+export pathResults=${path}/results/whole_genome_alignments/${dataset}
+
+#########################################################################
+
+if [ -e ${pathResults} ]; then
+    echo "dir exists"
+else
+    mkdir -p ${pathResults}
+fi
+
+#########################################################################
+
+if [ ${dataset} = "no_protuberance" ]; then
+    export targetGenomes="Struthio_camelus_australis,Dromaius_novaehollandiae,Gallus_gallus,Meleagris_gallopavo,Penelope_pileata,Alectura_lathami,Anas_platyrhynchos_platyrhynchos,Grus_americana,Calidris_pugnax,Upupa_epops,Rhinopomastus_cyanomelas,Strix_occidentalis_caurina,Ficedula_albicollis,Aquila_chrysaetos_chrysaetos,Geospiza_fortis,Parus_major,Serinus_canaria,Strigops_habroptila,Coturnix_japonica"
+fi
+
+#########################################################################
+
+if [ ${chr} = "all" ]; then
+    docker run -v ${path}:/ifb/data/mydatalocal/IPLOSS --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/aln.maf  --targetGenomes ${targetGenomes} --refGenome Anas_platyrhynchos_platyrhynchos --numProc ${nthreads} --noDupes --splitBySequence --smallSize 100000
+else
+    docker run -v ${path}:/ifb/data/mydatalocal/IPLOSS --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/aln.maf  --targetGenomes ${targetGenomes} --refGenome Anas_platyrhynchos_platyrhynchos --refSequence ${chr} --start=${start} --length=${length}  --noDupes 
+fi
+
+
+#########################################################################
