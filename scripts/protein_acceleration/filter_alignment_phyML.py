22a7a43b860c97640ba3ec9506f4c2a3e06a0628
Alexandre <alexandre.laverre@unil.ch>
2023-01-26 17:48:44 +0100

    Check if alignment still exists after cleaning

--- scripts/protein_acceleration/filter_alignment_phyML.py
+++ scripts/protein_acceleration/filter_alignment_phyML.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import sys
+import os
 from Bio import AlignIO
-from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 
@@ -14,34 +14,36 @@ PathResults = sys.argv[3]
 minLength = sys.argv[4]
 
 Part = IDList.split("/")[-1].strip(".txt")
-pathFiltered = PathResults + "/FilteredAlign/" + Part + "/"
+pathFiltered = PathResults + "/FilteredAlign/" + Part
 
 ###################################################################################################
-error_align = open(pathFiltered + "noalignment.txt", "a")
+error_align = open(pathFiltered + "_noalignment.txt", "a")
 
 with open(IDList, 'r') as f:
     for ID in f.readlines():
         ID = ID.strip("\n")
-        ids = []
-        seqs = []
+        AlignFile = PathAlign + "/" + ID + ".aln.best_hmm.fasta.fsa"
 
-        align = AlignIO.read(PathAlign + "/" + ID + ".aln.best_hmm.fasta.fsa", "fasta")
-        align_length = align.get_alignment_length()
+        if os.path.exists(AlignFile):  # HmmCleaner can return empty alignment
+            align = AlignIO.read(AlignFile, "fasta")
+            align_length = align.get_alignment_length()
 
-        if align_length > 0:
-            # Keep sequences with low number of GAP
-            for seq in align:
-                if seq.seq.count("-")/align_length < float(minLength):
-                    ids.append(seq.id)
-                    seqs.append(seq.seq)
+            if align_length > 0:  # Gblocks can return empty alignment
+                # Keep sequences with low number of GAP
+                ids = []
+                seqs = []
+                for seq in align:
+                    if seq.seq.count("-")/align_length < float(minLength):
+                        ids.append(seq.id)
+                        seqs.append(seq.seq)
 
-            records = (SeqRecord(s, id=i) for (i, s) in zip(ids, seqs))
+                records = (SeqRecord(s, id=i) for (i, s) in zip(ids, seqs))
 
-            if len(ids) > 1:
-                with open(pathFiltered + ID + ".rphylip", "w") as OutFile:
-                    AlignIO.write(MultipleSeqAlignment(records), OutFile, "phylip-relaxed")
-        else:
-            error_align.write(ID)
+                if len(ids) > 1:
+                    with open(pathFiltered + "/" + ID + ".rphylip", "w") as OutFile:
+                        AlignIO.write(MultipleSeqAlignment(records), OutFile, "phylip-relaxed")
+            else:
+                error_align.write(ID)
 
 error_align.close()
 ###################################################################################################
