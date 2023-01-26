9214fdfca072778fc217204e267ef0d322f4e7c8
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-01-26 16:11:05 +0100

    changed output dir

--- scripts/detect_conserved_elements/extract.alignments.sh
+++ scripts/detect_conserved_elements/extract.alignments.sh
@@ -20,14 +20,6 @@ export pathResults=${path}/results/whole_genome_alignments/${dataset}
 
 #########################################################################
 
-if [ -e ${pathResults} ]; then
-    echo "dir exists"
-else
-    mkdir -p ${pathResults}
-fi
-
-#########################################################################
-
 if [ ${dataset} = "no_protuberance" ]; then
     export targetGenomes="Struthio_camelus,Dromaius_novaehollandiae,Gallus_gallus,Meleagris_gallopavo,Penelope_pileata,Alectura_lathami,Anas_platyrhynchos_platyrhynchos,Grus_americana,Calidris_pugnax,Upupa_epops,Rhinopomastus_cyanomelas,Strix_occidentalis,Ficedula_albicollis,Aquila_chrysaetos,Geospiza_fortis,Parus_major,Serinus_canaria,Coturnix_japonica"
 
@@ -42,10 +34,18 @@ fi
 
 #########################################################################
 
-if [ ${chr} = "all" ]; then
-    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/aln.maf  --targetGenomes ${targetGenomes} --refGenome ${refGenome} --numProc ${nthreads} --noDupes --splitBySequence --smallSize 100000
+if [ -e ${pathResults}/${refGenome} ]; then
+    echo "dir exists"
 else
-    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/aln.maf  --targetGenomes ${targetGenomes} --refGenome ${refGenome} --refSequence ${chr} --start=${start} --length=${length}  --noDupes 
+    mkdir -p ${pathResults}/${refGenome}
+fi
+
+#########################################################################
+
+if [ ${chr} = "all" ]; then
+    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/${refGenome}/aln.maf  --targetGenomes ${targetGenomes} --refGenome ${refGenome} --numProc ${nthreads} --noDupes --splitBySequence --smallSize 100000
+else
+    docker run -v ${path}:/ifb/data/mydatalocal/HelmetedCurassowGenome --rm -t quay.io/comparative-genomics-toolkit/cactus:v1.3.0 hal2mafMP.py ${pathHAL}/366-avian.hal ${pathResults}/${refGenome}/aln.maf  --targetGenomes ${targetGenomes} --refGenome ${refGenome} --refSequence ${chr} --start=${start} --length=${length}  --noDupes 
 fi
 
 
