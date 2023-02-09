b7653b4995456df2acf4400e6d6a33723a7956ff
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-02-09 15:37:55 +0100

    simplify input files for pelican

--- scripts/convergent_aa_profiles/prepare.alignments.for.pelican.R
+++ scripts/convergent_aa_profiles/prepare.alignments.for.pelican.R
@@ -36,26 +36,14 @@ for(spset in c("all_species")){ ## other option is "without_chameleons"
 
         if(dataset == "all_species"){
             orthogroups=read.table(paste(pathOrthoGroups, "N0.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
-            refsp="Gallus_gallus"
-
-            names=read.table(paste(pathAnnot, "Chicken/GeneNames_Ensembl103.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
-            rownames(names)=names[,1]
         }
 
         if(dataset == "birds"){
             orthogroups=read.table(paste(pathOrthoGroups, "N2.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
-            refsp="Gallus_gallus"
-
-            names=read.table(paste(pathAnnot, "Chicken/GeneNames_Ensembl103.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
-            rownames(names)=names[,1]
         }
 
         if(dataset == "squamates"){
             orthogroups=read.table(paste(pathOrthoGroups, "N1.tsv",sep=""), h=T, stringsAsFactors=F, sep="\t")
-            refsp="Anolis_carolinensis"
-
-            names=read.table(paste(pathAnnot, "Anolis_carolinensis/GeneNames_Ensembl103.txt", sep=""), h=T, stringsAsFactors=F, sep="\t")
-            rownames(names)=names[,1]
         }
 
         ###########################################################################
@@ -65,34 +53,7 @@ for(spset in c("all_species")){ ## other option is "without_chameleons"
             og=orthogroups$OG[i]
 
             pathIn=paste(pathCDS, hog,"_",og,".aln.best.fas",sep="")
-
-            if(file.exists(pathIn)){
-                id.ref=orthogroups[i,refsp]
-
-                if(!is.na(id.ref) & id.ref!=""){
-                    id.ref=unlist(strsplit(id.ref, split="\\."))[1]
-
-                    print(id.ref)
-
-                    if(id.ref%in%rownames(names)){
-                        name.ref=names[id.ref, 2]
-
-                        if(length(grep("/",name.ref))!=0){
-                            name.ref=str_replace(name.ref, '/', '_')
-                        }
-
-                        if(!is.na(name.ref) & name.ref!=""){
-                            system(paste("cp ",pathIn, " ",pathOutput, "/",id.ref,"_",name.ref,".fa",sep=""))
-                        } else{
-                            system(paste("cp ",pathIn, " ",pathOutput, "/",id.ref,".fa",sep=""))
-                        }
-                    } else{
-                        system(paste("cp ",pathIn, " ",pathOutput, "/",hog,"_",og,".fa",sep=""))
-                    }
-                } else{
-                    system(paste("cp ",pathIn, " ",pathOutput, "/",hog,"_",og,".fa",sep=""))
-                }
-            }
+            system(paste("cp ",pathIn, " ",pathOutput, "/",hog,"_",og,".fa",sep=""))
 
             if(i%%1000==0){
                 print(i)
