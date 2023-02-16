1b65274958cca9f18fdc97e9ee18677fd0a8d6b9
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-02-16 20:39:22 +0100

    genes selected by max pval

--- scripts/convergent_aa_profiles/prepare.go.enrichment.R
+++ scripts/convergent_aa_profiles/prepare.go.enrichment.R
@@ -13,13 +13,22 @@ for(dataset in c("birds", "squamates")){
     all.sites=all.sites[which(all.sites$HumanGeneName!=""),]
     all.sites=all.sites[grep(",", all.sites$HumanGeneName, invert=T),]
 
+    ## sort genes by minimum p-value
     min.pval.gene=tapply(all.sites$aagtr_pval, as.factor(all.sites$HumanGeneName), min)
     df.min.pval.gene=data.frame("GeneName"=levels(as.factor(all.sites$HumanGeneName)), "MinPValue"=min.pval.gene)
     df.min.pval.gene=df.min.pval.gene[order(df.min.pval.gene$MinPValue),]
 
     write.table(df.min.pval.gene, file=paste(pathResults, dataset, "/pelican_output_general/minimum_pvalue_per_gene.tsv",sep=""),sep="\t", row.names=F, col.names=T)
 
-    writeLines(df.min.pval.gene$GeneName,con=paste(pathResults, dataset, "/pelican_output_general/genes_sorted_by_minimum_pvalue.txt",sep=""))
+    writeLines(df.min.pval.gene$GeneName, con=paste(pathResults, dataset, "/pelican_output_general/genes_sorted_by_minimum_pvalue.txt",sep=""))
+
+    ## extract genes that have at least one site with a p-value below a threshold
+
+    for(threshold in c(0.01, 0.001)){
+      selected.genes=df.min.pval.gene$GeneName[which(df.min.pval.gene$MinPValue<threshold)]
+      writeLines(selected.genes, con=paste(pathResults, dataset, "/pelican_output_general/genes_max_pvalue_",threshold,".txt",sep=""))
+    }
+    
 }
 
 ################################################################
