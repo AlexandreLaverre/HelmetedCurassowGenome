b022d96d9f1c4204803d48856763473d4d2ad09c
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-02-21 13:28:13 +0100

    added GC for ref sp

--- scripts/main_figures/data.GC.content.R
+++ scripts/main_figures/data.GC.content.R
@@ -26,6 +26,20 @@ for(i in 1:2){
 
 ######################################################################
 
+species=c("Gallus_gallus", "Anolis_carolinensis")
+
+for(i in 1:2){
+    sp=species[i]
+
+    pathGenome=paste("../../data/genome_sequences/Ensembl103/",sep="")
+
+    gcsize=read.table(paste(pathGenome, "GCContent_ChromosomeSize_",sp,".txt",sep=""), h=T, stringsAsFactors=F)
+
+    gcinfo[[sp]]=gcsize
+}
+
+######################################################################
+
 save(gcinfo, file=paste(pathFigures, "RData/gccontent.RData",sep=""))
 
 ######################################################################
