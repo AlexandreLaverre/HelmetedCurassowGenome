e73d3abdd39ad4688b246c68e0b53a1b22b06505
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2023-02-28 09:53:33 +0100

    format gene families

---
+++ scripts/gene_family_evolution/format.gene.families.R
@@ -0,0 +1,26 @@
+###################################################################################
+
+pathOrthoFinder="../../results/gene_families/OrthoFinder/all_species/iqtree/Results_Jan05/Phylogenetic_Hierarchical_Orthogroups/"
+
+pathResults="../../results/gene_family_evolution/all_species/birds/"
+
+###################################################################################
+
+og=read.table(paste(pathOrthoFinder, "N2.tsv", sep=""), h=T, stringsAsFactors=F, sep="\t")
+rownames(og)=paste(og$HOG, og$OG, sep="_")
+
+og.annot=read.table(paste(pathOrthoFinder, "/N2_annotations.tsv", sep=""), h=T, stringsAsFactors=F, sep="\t")
+rownames(og.annot)=paste(og.annot$HOG, og.annot$OG, sep="_")
+
+###################################################################################
+
+splist=colnames(og)[4:ncol(og)]
+
+nbgenes=t(apply(og[,splist], 1, function(x) unlist(lapply(x, function(y) length(unlist(strsplit(y, split=",")))))))
+
+###################################################################################
+
+cafe5.input=data.frame("Desc"=og.annot[rownames(og), "HumanOrthologue.GeneName"], "Family ID"=rownames(og), nbgenes)
+cafe5.input$Desc[which(cafe5$Input=="")]="NA"
+
+###################################################################################
